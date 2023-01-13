abe56c14d2445157ef6af526a725de9b7c471dfa
Reward Edoyugbo <rewardraj@gmail.com>
2023-01-13 14:25:54 +0100

    added contact section, little tweaks on collabs

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ let cursor = document.getElementById('cursor');
 document.onmousemove = function(e){
     cursor.style.left = (e.pageX - 25) + "px";
     cursor.style.top = (e.pageY - 25) + "px";
-    cursor.style.display = "block";
+    // cursor.style.display = "block";
 }
 
 // TAB toggle
